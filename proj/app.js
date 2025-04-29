d2f2d8a680e98916127c0fdec73b4f4a03b8d007
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2025-04-29 12:51:25 -0300

    Reorganizando e uso do Handlebars

--- proj/app.js
+++ proj/app.js
@@ -1,33 +1,31 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import express from 'express';
+import { engine } from 'express-handlebars';
+import Sequelize from 'sequelize';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const app = express();
+// Config
+    // Files
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    // Express
+    const app = express();
+    // Template Engine
+    app.engine('handlebars', engine({defaultLayout: 'main'}));
+    app.set('view engine', 'handlebars');
+    // Database
+    const sequelize = new Sequelize('cadastrosexpress', 'root', '', {
+        host: 'localhost',
+        dialect: 'mysql'
+    });
 
-// Rotas
-app.get('/', (req, res) => {
-    res.send('Seja bem-vindo à minha aplicação Express.js!');
+// Rotes
+app.get('/cadastrar-usuario', (req, res) => {
+    res.render('usuario_form');
 });
 
-app.get('/hello', (req, res) => {
-    res.send('Olá, mundo!');
-});
 
-app.get('/hello/:nome', (req, res) => {
-    res.send('Olá, ' + req.params.nome + '!');
-});
-
-app.get('/hello-html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'html', 'hello.html'));
-});
-
-app.get('/sobre', (req, res) => {
-    res.send('Esta é uma aplicação simples sendo desenvolvida em Express.js.');
-});
-
-// Iniciando o server
+// Server start
 app.listen(3000, () => {
     console.log('O server está rodando!');
 });

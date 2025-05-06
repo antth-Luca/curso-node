01e3929ffa4facd82868d3b3543a2e6af8dbc69a
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2025-05-06 10:14:28 -0300

    Editando registro

--- final-project/routes/admin/categoria.js
+++ final-project/routes/admin/categoria.js
@@ -2,7 +2,7 @@ import router from '../Router.js';
 
 import Categoria from '../../database/models/Categoria.js';
 
-
+// Read
 router.get('/lista', (req, res) => {
     Categoria.find().then((categorias) => {
         res.render(
@@ -12,7 +12,7 @@ router.get('/lista', (req, res) => {
         req.flash('error_msg', 'Houve um erro');
     });
 });
-
+// Create
 router.get('/criar', (req, res) => {
     res.render('admin/categoria/categoria_criar');
 });
@@ -29,6 +29,31 @@ router.post('/criar', (req, res) => {
         }).catch((error) => {
             console.log('Erro ao salvar: ' + error);
         });
+});
+// Update
+router.get('/editar/:id', (req, res) => {
+    Categoria.findOne({_id: req.params.id}).then((categoria => {
+        res.render(
+            'admin/categoria/categoria_editar',
+            {categoria: categoria});
+    })).catch((error) => {
+        req.flash('error_msg', 'Houve um erro');
+    });
+});
+
+router.post('/editar/:id', (req, res) => {
+    Categoria.findOne({_id: req.params.id}).then((categoria => {
+        categoria.nome = req.body.nome
+        categoria.nome_slug = req.body.nome_slug
+
+        categoria.save().then(() => {
+            res.redirect('/admin/categoria/lista');
+        }).catch((error) => {
+            req.flash('error_msg', 'Houve um erro');
+        });
+    })).catch((error) => {
+        req.flash('error_msg', 'Houve um erro');
+    });
 });
 
 export default router;
